9bb9af2befc0e3ed7a125948681eedd5f99e8b79
toxypiks <HannaHinkelbein89@gmx.de>
2025-04-08 09:56:51 +0200

    Include library for hashmap and dynamic array and print string pair-wise

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
 
 int main(void)
 {
-    printf("Ready set go!\n");
+    const char *text = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text.";
+    int text_size = strlen(text);
+
+    for (int i = 0; i < text_size; ++i) {
+        char a = text[i];
+        char b = text[i + 1];
+        printf("%c%c\n", a, b);
+    }
     return 0;
 }
