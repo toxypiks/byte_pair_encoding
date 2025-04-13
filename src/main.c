ef68dfd40941028646b9cf0cd171476d2d5fc28d
toxypiks <HannaHinkelbein89@gmx.de>
2025-04-13 10:11:27 +0200

    Repeat compressing procedure and print final text length

--- src/main.c
+++ src/main.c
@@ -35,6 +35,13 @@ int compare_freqs(const void *a, const void *b)
     return (int)bf->value - (int)af->value;
 }
 
+#define swap(Type, x, y) \
+    do { \
+        Type t = (x); \
+        (x) = (y); \
+        (y) = t; \
+    } while (0)
+
 int main(void)
 {
     const char *text = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text.";
@@ -58,63 +65,62 @@ int main(void)
         arrput(tokens_in, text[i]);
     }
 
-    // Put two chars of token_in into Pair and put in Hashmap if not already there, if there increment counter for pair in hashmap (value)
-    for (size_t i = 0; i < arrlen(tokens_in) - 1; ++i) {
-        Pair pair = {
-            .l = tokens_in[i],
-            .r = tokens_in[i+1]
-        };
-        ptrdiff_t i = hmgeti(freq, pair);
-        if (i < 0) hmput(freq, pair, 1);
-        else freq[i].value += 1;
-    }
+    for (;;) {
+        // render_tokens(pairs, tokens_in); // Render tokens throughout each compression iteration
 
-    // Put pairs and occurence in freqs_sorted array
-    for (ptrdiff_t i = 0; i < hmlen(freq); ++i) {
-        arrput(freqs_sorted, freq[i]);
-    }
+        printf("%zu\n", arrlen(tokens_in));
 
-    // Find index of pair with max occurence in hashmap
-    ptrdiff_t max_index = 0;
-    for (ptrdiff_t i = 1; i < hmlen(freq); ++i) {
-        if (freq[i].value > freq[max_index].value) {
-            max_index = i;
+        hmfree(freq);
+        // Put two chars of token_in into Pair and put in Hashmap if not already there, if there increment counter for pair in hashmap (value)
+        for (size_t i = 0; i < arrlen(tokens_in) - 1; ++i) {
+            Pair pair = {
+                .l = tokens_in[i],
+                .r = tokens_in[i+1]
+            };
+            ptrdiff_t i = hmgeti(freq, pair);
+            if (i < 0) hmput(freq, pair, 1);
+            else freq[i].value += 1;
         }
-    }
 
-    printf("(%u, %u) => %zu\n", freq[max_index].key.l, freq[max_index].key.r, freq[max_index].value);
+        // Put pairs and occurence in freqs_sorted array
+        for (ptrdiff_t i = 0; i < hmlen(freq); ++i) {
+            arrput(freqs_sorted, freq[i]);
+        }
 
-    // Put pair with max occurence in pairs array at new index (first time its index 256)
-    arrput(pairs, freq[max_index].key);
-
-    // find pair with max occurence in tokens_in and replace it with token and put it into tokens_out otherwise just put pairs into tokens_out
-    for (size_t i = 0; i < arrlen(tokens_in);) {
-        if (i + 1 >= arrlen(tokens_in)) {
-            arrput(tokens_out, tokens_in[i]);
-            i += 1;
-        } else {
-            Pair pair = {.l = tokens_in[i], .r = tokens_in[i + 1]};
-            if (memcmp(&pair, &freq[max_index].key, sizeof(pair)) == 0) {
-                arrput(tokens_out, arrlen(pairs) - 1);
-                i += 2;
-            } else {
-                arrput(tokens_out, tokens_in[i]);
-                i += 1;
+        // Find index of pair with max occurence in hashmap
+        ptrdiff_t max_index = 0;
+        for (ptrdiff_t i = 1; i < hmlen(freq); ++i) {
+            if (freq[i].value > freq[max_index].value) {
+                max_index = i;
             }
         }
+
+        if (freq[max_index].value <= 1) break; // No further compression can be done
+
+        // Put pair with max occurence in pairs array at new index (first time its index 256)
+        arrput(pairs, freq[max_index].key);
+
+        // clean up tokens_out for next compressing iteration
+        arrfree(tokens_out);
+        uint32_t *tokens_out = NULL;
+
+        // find pair with max occurence in tokens_in and replace it with token and put it into tokens_out otherwise just put pairs into tokens_out
+        for (size_t i = 0; i < arrlen(tokens_in);) {
+            if (i + 1 >= arrlen(tokens_in)) {
+                arrput(tokens_out, tokens_in[i]);
+                i += 1;
+            } else {
+                Pair pair = {.l = tokens_in[i], .r = tokens_in[i + 1]};
+                if (memcmp(&pair, &freq[max_index].key, sizeof(pair)) == 0) {
+                    arrput(tokens_out, arrlen(pairs) - 1);
+                    i += 2;
+                } else {
+                    arrput(tokens_out, tokens_in[i]);
+                    i += 1;
+                }
+            }
+        }
+        swap(uint32_t*, tokens_in, tokens_out);
     }
-
-    render_tokens(pairs, tokens_in);
-    render_tokens(pairs, tokens_out);
-
-    printf("Uncompressed text size: %d\n", arrlen(tokens_in));
-    printf("Compressed text size: %d\n", arrlen(tokens_out));
-
-    // qsort(freqs_sorted, arrlen(freqs_sorted), sizeof(Freq), compare_freqs);
-    //
-    // for (size_t i = 0; i < arrlen(freqs_sorted); ++i) {
-    //     Freq *freqs = &freqs_sorted[i];
-    //     printf("(%u, %u) => %zu\n", freqs->key.l, freqs->key.r, freqs->value);
-    // }
     return 0;
 }
