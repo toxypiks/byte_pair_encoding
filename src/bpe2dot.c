9fff87d9c4700355c7fb11c5d7e1af71a90c4f53
toxypiks <HannaHinkelbein89@gmx.de>
2025-04-19 10:39:35 +0200

    Add usage function to print info when program is not used correctly

--- src/bpe2dot.c
+++ src/bpe2dot.c
@@ -108,19 +108,26 @@ defer:
 
 }
 
+void usage(const char *program_name)
+{
+    fprintf(stderr, "Usage %s <input.bpe> <output.dot>\n", program_name);
+}
+
 int main(int argc, char **argv)
 {
     const char* program_name = argv[0];
 
     if (argc <= 1) {
-        fprintf(stderr, "Usage: %s <input.bin> <output.dot>\n", program_name);
+        usage(program_name);
         fprintf(stderr, "ERROR: no input is provided\n");
+        return 1;
     }
     const char* input_file_path = argv[1];
 
     if (argc == 2) {
-        fprintf(stderr, "Usage: %s <input.bin> <output.dot>\n", program_name);
+        usage(program_name);
         fprintf(stderr, "ERROR: no output is provided\n");
+        return 1;
     }
     const char* output_file_path = argv[2];
 
